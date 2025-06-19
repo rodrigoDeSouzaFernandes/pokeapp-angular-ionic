01e7f34c0d1b688e1c8d7d9ed1f1b4b74a160310
Rodrigo <rdesouza2008@gmaill.com>
2025-06-19 19:55:27 -0300

    (testes) cria testes para o meu service PokeapiService

--- src/app/services/pokeapi.service.spec.ts
+++ src/app/services/pokeapi.service.spec.ts
@@ -1,16 +1,55 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { PokeapiService } from './pokeapi.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-// import { PokeapiService } from './pokeapi.service';
+describe('PokeapiService', () => {
+  let service: PokeapiService;
+  let httpMock: HttpTestingController;
 
-// describe('PokeapiService', () => {
-//   let service: PokeapiService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PokeapiService],
+    });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(PokeapiService);
-//   });
+    service = TestBed.inject(PokeapiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  afterEach(() => {
+    httpMock.verify(); // Garante que não ficaram requisições pendentes
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should fetch list of pokemons with limit and offset', () => {
+    const mockResponse = { results: [{ name: 'bulbasaur' }] };
+
+    service.getPokemons(10, 0).subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(
+      'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should fetch pokemon details by name', () => {
+    const mockDetail = { name: 'bulbasaur', id: 1 };
+
+    service.getPokemonDetails('bulbasaur').subscribe((res) => {
+      expect(res).toEqual(mockDetail);
+    });
+
+    const req = httpMock.expectOne('https://pokeapi.co/api/v2/pokemon/bulbasaur');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockDetail);
+  });
+});
